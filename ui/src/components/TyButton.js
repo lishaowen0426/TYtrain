94f4bc0630d426c1705e85b653bec4768e9cc865
Li Shaowen <litony0426@gmail.com>
2023-11-14 16:07:22 +0900

    update

--- ui/src/components/TyButton.js
+++ ui/src/components/TyButton.js
@@ -2,10 +2,5 @@ import * as React from "react";
 import Button from "react-bootstrap/Button";
 
 export const TySubmitButton = props => {
-  return (
-    <Button
-      className="ty-button disabled:opacity-50 focus:outline-0 hover:bg-sky-800 active:bg-sky-900"
-      {...props}
-    ></Button>
-  );
+  return <Button className="bg-sky-800" {...props}></Button>;
 };
